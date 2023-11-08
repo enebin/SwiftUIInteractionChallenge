b31cc0219ff97d7ed2725710453e865dbb7ce176
YoungBin Lee <enebin777@gmail.com>
2023-11-08 11:58:50 +0900

    Initial Commit

---
+++ SwiftUIInteractionChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIInteractionChallenge
+//
+//  Created by ab180 on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIInteractionChallenge/SwiftUIInteractionChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInteractionChallengeApp.swift
+//  SwiftUIInteractionChallenge
+//
+//  Created by ab180 on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInteractionChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
